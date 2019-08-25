8f626d21281327556b038a67d9b002152e86dcb4
poznas <misterdannypl@gmail.com>
2019-08-25 18:30:32 +0200

    setup helm & jenkins as k8s service

---
+++ install-jenkins.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>/home/ec2-user/jenkins-init.log 2>&1
+
+aws ec2 create-volume --size=60 --volume-type=gp2 \
+    --availability-zone=$(awk -F, '{print $1}' <<< $AWS_AVAILABILITY_ZONES) \
+    --tag-specifications 'ResourceType=volume,Tags=[{Key=KubernetesCluster,Value='"$NAME"'}]' \
+    > ebsVolume.json
+
+export JENKINS_EBS_VOL_ID=$(jq -r '.VolumeId' ebsVolume.json)
+export JENKINS_ADMIN_PASS=$(jq -r '.AdminPassword' /home/ec2-user/config/secrets/jenkins-secrets.json)
+
+sed -ri 's/^(\s*)(volumeID\s*:\s*<X>\s*$)/\1volumeID: '"$JENKINS_EBS_VOL_ID"'/' \
+    /home/ec2-user/config/k8s/pv-jenkins-home.yaml
+
+sed -ri 's/^(\s*)(adminPassword\s*:\s*<X>\s*$)/\1adminPassword: '"$JENKINS_ADMIN_PASS"'/' \
+    /home/ec2-user/config/k8s/jenkins-values.yaml
+
+kubectl --context=${NAME} apply -f /home/ec2-user/config/k8s/pv-jenkins-home.yaml
+
+kubectl create serviceaccount --namespace kube-system tiller
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+
+#helm install --name jenkins-stg --kube-context=${NAME} -f /home/ec2-user/config/k8s/jenkins-values.yaml stable/jenkins
+
+##### helm del --purge jenkins-stg
--- k8s-bootstrap-init.sh
+++ k8s-bootstrap-init.sh
@@ -1,4 +1,7 @@
-exec > >(tee /home/ec2-user/init-log.log|logger -t init-log ) 2>&1
+#!/bin/bash
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>/home/ec2-user/init-log.log 2>&1
 
 echo $(pwd) > /home/ec2-user/init-pwd.out
 
@@ -12,6 +15,16 @@ mv ./kubectl /usr/local/bin/kubectl
 curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x ./kops
 mv ./kops /usr/local/bin/
+
+wget https://get.helm.sh/helm-v2.14.3-linux-amd64.tar.gz
+tar zxvf helm-v2.14.3-linux-amd64.tar.gz
+mv linux-amd64/helm /usr/local/bin/
+rm helm-v2.14.3-linux-amd64.tar.gz
+rm -rf ./linux-amd64/
+
+chmod +x /home/ec2-user/config/install-jenkins.sh
+
+chown -R ec2-user:ec2-user /home/ec2-user/config
 
 #aws iam create-group --group-name kops
 
@@ -32,8 +45,9 @@ aws iam create-access-key --user-name kops > kopsCreds.json
 export AWS_ACCESS_KEY_ID=$(jq -r '.AccessKey.AccessKeyId' kopsCreds.json)
 export AWS_SECRET_ACCESS_KEY=$(jq -r '.AccessKey.SecretAccessKey' kopsCreds.json)
 export AWS_DEFAULT_REGION=eu-central-1
-export AWS_AVAILABILITY_ZONES=eu-central-1a,eu-central-1b,eu-central-1c
+export AWS_AVAILABILITY_ZONES=eu-central-1a,eu-central-1b
 
+sleep 30s
 
 #aws s3api create-bucket \
 #    --bucket surf-advisor-state-store \
@@ -50,10 +64,15 @@ printf '\n' >> /etc/profile.d/env-setup.sh
 printenv | sed -e 's/^/export /g' >> /etc/profile.d/env-setup.sh
 
 ssh-keygen -b 2048 -t rsa -f /home/ec2-user/.ssh/id_rsa -q -N ""
+
 kops create secret --name ${NAME} sshpublickey admin -i /home/ec2-user/.ssh/id_rsa.pub
 
 kops create cluster  --name ${NAME} --zones $(echo "$AWS_AVAILABILITY_ZONES") \
     --master-size m3.medium --master-count 1 \
     --node-size t3.medium --node-count 2
 
-##### kops update cluster ${NAME} --yes
+kops update cluster ${NAME} --yes
+
+# sleep 8m; helm init; sleep 30s; bash /home/ec2-user/config/install-jenkins.sh
+
+#### kops delete cluster --name ${NAME} --yes
