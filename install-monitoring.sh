be3e97baa8016521a5e76cc1bb4bb50b92f3069c
poznas <misterdannypl@gmail.com>
2019-11-09 18:02:33 +0100

    fix `namespaces "monitoring" not found` while applying grafana config

--- install-monitoring.sh
+++ install-monitoring.sh
@@ -3,8 +3,6 @@ exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>/home/ec2-user/monitoring-init.log 2>&1
 
-kubectl --context=${NAME} apply -f /home/ec2-user/config/k8s/monitoring/configmap/grafana-config.yaml
-
 sleep 5s;
 
 helm install stable/prometheus --namespace monitoring --name prometheus
@@ -15,6 +13,8 @@ helm install stable/grafana \
     -f /home/ec2-user/config/k8s/monitoring/grafana-chart-values.yaml \
     --namespace monitoring \
     --name grafana
+
+kubectl --context=${NAME} apply -f /home/ec2-user/config/k8s/monitoring/configmap/grafana-config.yaml
 
 #kubectl get secret --namespace monitoring grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
 
